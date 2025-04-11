aa6133f139282d5f7b97796baca36c0e11ebb990
Dharyansh Achlas <salchad27@MacDA.local>
2025-04-12 01:12:28 +0530

    nextconfigjsChange

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: isProd ? '/blackbucknationalpark/' : '',
+  basePath: isProd ? '/blackbucknationalpark' : '',
+  images: {
+    unoptimized: true, // only needed if you're using next/image
+  },
+  trailingSlash: true, 
 };
 
 export default nextConfig;
