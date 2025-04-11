b3f9d173ae0b8ca077203e9e2627b7378f4e8f3d
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-04-12 02:33:21 +0530

    Update page.tsx

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
         <motion.div
           className="absolute inset-0 w-full h-full"
           style={{
-            backgroundImage: "url('/abtHomeImg.png')",
+            backgroundImage: "./abtHomeImg.png",
             backgroundPosition: "center",
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
